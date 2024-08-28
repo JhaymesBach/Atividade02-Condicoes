53fe03f9373e6a10bdc3d0120095c381c3838525
iurigiroldi <iurigiroldi@gmail.com>
2024-08-28 20:16:14 -0300

    ll

---
+++ src/VerificacaoDeMaioridade.java
@@ -0,0 +1,26 @@
+public class VerificacaoDeMaioridade {
+
+public static void main(String[] args) {
+
+    Integer idade = 19;
+
+    if (idade >= 18) {
+        System.out.println("É maior que 18");
+    } else {
+        System.out.println("Não é maior que 18");
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+}
